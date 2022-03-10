9829e724f7f26a56d96c8fb7924095eab6b5dd5f
DanielRenz <daniel.renz98@yahoo.de>
2022-03-10 18:44:02 +0100

    on off switch with sending a message

--- content_scripts/highlight.js
+++ content_scripts/highlight.js
@@ -1,1 +1,11 @@
 console.log("highlight running");
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.on == true) {
+        console.log("Ext is on");
+    } else {
+        console.log("Ext is off");
+    }
+
+    sendResponse("200");
+});
--- extension_window/popup.js
+++ extension_window/popup.js
@@ -1,1 +1,13 @@
 console.log("popup running!");
+
+const onoff = document.querySelector("input[name=checkbox");
+
+function isOn() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {on: onoff.checked}, function(response) {
+          console.log(response);
+        });
+    });
+}
+
+onoff.addEventListener("click", isOn);
