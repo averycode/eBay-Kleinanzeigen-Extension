b75134373fbf4136b619d451d2bb8af292a2a03e
DanielRenz <daniel.renz98@yahoo.de>
2022-03-11 17:46:04 +0100

    seller profile box dom manipulation und hover funktionalität

--- content_scripts/highlight.js
+++ content_scripts/highlight.js
@@ -1,11 +1,54 @@
 console.log("highlight running");
 
+const style = document.createElement("style");
+style.innerHTML = `
+      .profileBox {
+          border: 3px solid yellow;
+          background-color: #ff00ff60;
+      }
+      .profileBox-mouseover {
+          border: 6px solid yellow;
+          background-color: #ff00ff60;
+      }
+    `;
+document.head.appendChild(style);
+
+let running = true;
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.on == true) {
-        console.log("Ext is on");
+        running = true;
+        runScript();
     } else {
-        console.log("Ext is off");
+        running = false;
+        runScript();
     }
 
     sendResponse("200");
 });
+
+// DOM Elements
+let profileBox = document.querySelector("#viewad-profile-box");
+profileBox.addEventListener("mouseover", mouseover_profileBox);
+profileBox.addEventListener("mouseout", mouseout_profileBox);
+
+// everything in here what has to do with on and off
+function runScript() {
+    if (running == true) {
+        profileBox.classList.add("profileBox");
+    } else {
+        profileBox.classList.remove("profileBox");
+    }
+}
+
+function mouseover_profileBox() {
+    if (running) {
+        profileBox.classList.add("profileBox-mouseover");
+    }
+}
+
+function mouseout_profileBox() {
+    if (running) {
+        profileBox.classList.remove("profileBox-mouseover");
+    }
+}
--- extension_window/popup.js
+++ extension_window/popup.js
@@ -1,13 +1,19 @@
 console.log("popup running!");
 
 const onoff = document.querySelector("input[name=checkbox");
+onoff.checked = true;
 
 function isOn() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {on: onoff.checked}, function(response) {
-          console.log(response);
-        });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            { on: onoff.checked },
+            function (response) {
+                console.log(response);
+            }
+        );
     });
 }
 
+isOn();
 onoff.addEventListener("click", isOn);
